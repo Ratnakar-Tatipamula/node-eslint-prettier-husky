16d4afdb96bc4a5851d0285fff71a0242cbb65f4
Ratnakar-Tatipamula <47071239+Ratnakar-Tatipamula@users.noreply.github.com>
2022-11-02 12:22:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-eslint-prettier-husky
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-cncywr)
---
+++ index.js
@@ -0,0 +1,17 @@
+// import { foo } from './Test';
+
+var age = 25;
+
+function welcome() {
+  return 'Hello Node';
+}
+
+var isBoolean = true;
+
+if (isBoolean) {
+  console.log('hii');
+}
+
+console.log(age);
+
+welcome();
